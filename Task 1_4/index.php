f590d808f793f43b4a9ef51b0024c0706c374d74
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2022-03-30 15:15:09 +0500

    Task num 4

--- Task 1_4/index.php
+++ Task 1_4/index.php
@@ -1,1 +1,33 @@
 <?php
+$filename = __DIR__ . "/data.json";
+$result = [];
+if (file_exists($filename))
+    $result = json_decode(file_get_contents($filename), true);
+
+
+if(isset($_GET['set_viewed'])){
+    foreach ($result as &$item){
+        $viewedId = $_GET['set_viewed'];
+        if($item['id'] == $viewedId){
+            $item['viewed']++;
+        }
+    }
+
+    file_put_contents($filename, json_encode($result));
+}
+
+
+
+?>
+
+<? foreach ($result as $task): ?>
+    <div>
+        <b><?= $task['name'] ?></b><br>
+        <small>Viewed: <?= $task['viewed'] ?></small><br>
+        <?= $task['text'] ?><br><br>
+        <a href="?set_viewed=<?= $task['id'] ?>">I watched</a>
+        <hr>
+    </div>
+
+<? endforeach;?>
+
